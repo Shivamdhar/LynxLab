763d0c859b0157fc832bdb3bb80ce12388374af1
Name-X <skvarun1995@gmail.com>
2026-05-21 13:19:11 +0530

    Using unique name for S3 bucket

--- terraform/README.md
+++ terraform/README.md
@@ -1,7 +1,7 @@
 ## Steps to spin up the stack
 1. terraform init
-2. terraform apply -var="auth_secret_value=$(aws configure get aws_secret_access_key)" -var="bucket_name=<unique-bucket-name>"
+2. terraform apply -var="auth_secret_value=$(aws configure get aws_secret_access_key)" -auto-approve
 
 
 ## Steps to destroy up the stack
-1. terraform destroy
+1. terraform destroy -auto-approve
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,4 +1,10 @@
+# 1. Automatically grab the identity of whoever runs the command
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "logs_bucket" {
-  bucket = var.bucket_name
+  # 2. Extract the IAM user/role name dynamically and use it as a prefix
+  # (lower() and split() ensure compliance with S3's strict lowercase naming rules)
+  bucket_prefix = "logs-${lower(element(split("/", data.aws_caller_identity.current.arn), 1))}-"
+  
   force_destroy = true
 }
